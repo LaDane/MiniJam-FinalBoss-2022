52c1de29b3c7bcef3e66c3a0d62e084ba0e40391
Marcus Due <emhen16@student.sdu.dk>
2022-08-21 12:37:59 +0200

    Added boss music

---
+++ MiniJam-FinalBoss-2022/Assets/Scripts/Music/GlobalVolume.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalVolume : MonoBehaviour
+{
+
+    [SerializeField] private AudioClip audioClip1;
+    [SerializeField] private AudioClip audioClip2;
+    [SerializeField] private AudioClip audioClip3;
+    [SerializeField] private AudioClip audioClip4;
+    [SerializeField] private AudioClip audioClip5;
+
+    [SerializeField] private AudioSource audioSource;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+       
+}
